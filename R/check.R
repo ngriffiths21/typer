0fa2dafcb5fe1b1da2a14cc7f041ab97fc7528e2
Nick Griffiths <ngriffiths21@gmail.com>
2020-02-08 14:27:25 -0500

    better output error formatting in basic list
    - results returned as a list of result objects
    - testing updated to match new structure

--- R/check.R
+++ R/check.R
@@ -30,37 +30,42 @@ get_type_errors <- function (filename) {
 }
 
 validate_types <- function (typedefs) {
-  result <- lapply(typedefs, function (x) {
-    validate_type(extract_fn_body(x$call), x$params, typedefs)
+  result <- map(typedefs, function (x) {
+    validate_type(extract_fn_body(x$call), x$params, typedefs, x$alias)
   })
-  unlist(result)
+  result <- flatten(result)
+  result
 }
 
 extract_fn_body <- function (call) {
   call[[3]][[3]][[-1]]
 }
 
-validate_type <- function (expra, paramtypes, alltypes) {
+validate_type <- function (expra, paramtypes, alltypes, currfun) {
   if (rlang::is_syntactic_literal(expra)) {
-    "valid literal"
+    list(type_response(message, str_c("valid literal `", expra, "`"), fn = currfun))
   } else if (is.call(expra)) {
-    validate_call(expra, paramtypes, alltypes)
+    validate_call(expra, paramtypes, alltypes, currfun)
+  } else if (is.symbol(expra)) {
+    list(type_response(message, str_c("valid symbol `", expra, "`"), fn = currfun))
   } else {
-    "can't tell"
+    list(type_response(warning, str_c("can't tell how to check `", expra, "`"), fn = currfun))
   }
 }
 
-validate_call <- function (call, paramtypes, alltypes) {
+validate_call <- function (call, paramtypes, alltypes, currfun) {
   append(
-    call_is_correct(
-      append(
-        call[[1]], // must pluck element to reconstruct a call
-        map(call[-1], ~ make_literal(., alltypes, paramtypes))
-      ),
-      paramtypes,
-      alltypes
+    list(
+      call_is_correct(
+        append(
+          call[[1]], # must pluck element to reconstruct a call
+          map(call[-1], ~ make_literal(., alltypes, paramtypes))
+        ),
+        paramtypes,
+        alltypes
+      )
     ),
-    map(call[-1], ~ validate_type(., paramtypes, alltypes))
+    flatten(map(call[-1], ~ validate_type(., paramtypes, alltypes, currfun)))
   )
 }
 
@@ -79,6 +84,9 @@ make_literal <- function (expra, allexprs, paramtypes) {
       return(expra)
     }
   } else {
+    if (!rlang::is_syntactic_literal(expra)) {
+      warning("Typer cannot determine how to check expression `", expra, "`.")
+    }
     expra
   }
 }
@@ -86,11 +94,33 @@ make_literal <- function (expra, allexprs, paramtypes) {
 call_is_correct <- function (literalcall, paramtypes, alltypes) {
   literalcall <- as.call(literalcall)
   knownfun <- alltypes[[rlang::as_string(literalcall[[1]])]]
+  strfun <- as.list(literalcall)[[1]]
 
   if (!is.null(knownfun)) {
     expandedcall <- match_call(eval(knownfun$call), literalcall)
     paramvals <- imap_chr(knownfun$params, ~ typeof(expandedcall[[.y]]))
     types <- map_chr(knownfun$params, ~ .)
-    ifelse(types == paramvals, "ok", "error")
+
+    if(types == paramvals) {
+      type_response(message, str_c("call to `", strfun, "` is correct"),
+                    list(fn = strfun, type = "call", call = expandedcall, types = types, passed = TRUE))
+    } else {
+      type_response(collect_error, str_c("error in call to `", strfun, "`!", " wanted ", types, "; got ", paramvals),
+                    list(fn = strfun, type = "call", call = expandedcall, types = types, passed = FALSE))
+    }
+  } else {
+    type_response(warning, str_c("cannot check function `", strfun, "` because it is not defined."), strfun)
   }
+}
+
+collect_error <- function (text) {
+  warning(text)
+}
+
+type_response <- function(reporter, text, comparison = NULL, fn = NULL) {
+  do.call(reporter, list(text))
+  if (!is.null(comparison)) {
+    return(comparison)
+  }
+  list(fn = fn, type = "message", text = text)
 }
--- tests/testthat/test_typedefs.R
+++ tests/testthat/test_typedefs.R
@@ -1,27 +1,30 @@
 context("Parsing typedefs")
+
+test_file_errs <- get_type_errors("../example.R")
+
 test_that("It can get typedefs from an example file", {
   expect_type(get_file_typedefs("../example.R"), "list")
 })
 
 test_that("It matches literals to types in add_test", {
-  expect_equal(get_type_errors("../example.R")[["add_test.x"]], "ok")
+  expect_true(test_file_errs[[12]]$passed)
 })
 
 test_that("It matches return values to types in add_test", {
-  expect_equal(get_type_errors("../example.R")[["add_test.x"]], "ok")
+  expect_true(test_file_errs[[7]]$passed)
 })
 
 test_that("It matches parameters to types in add_eight", {
-  expect_equal(get_type_errors("../example.R")[["add_eight.x"]], "ok")
+  expect_true(test_file_errs[[11]]$passed)
 })
 
 test_that("It detects an error in add_wrong", {
-  expect_equal(get_type_errors("../example.R")[["add_wrong.x"]], "error")
+  expect_false(test_file_errs[[15]]$passed)
 })
 
 
 test_that("It detects mismatched parameters", {
-  expect_equal(get_type_errors("../example.R")[["add_wrongparam.x"]], "error")
+  expect_false(test_file_errs[[17]]$passed)
 })
 
 test_that("It matches types of variables in the environment", {
